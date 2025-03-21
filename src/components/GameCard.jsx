a01d9d39349dc7f6672b9c12c83315414a817774
JSLegend <jslegend@protonmail.com>
2025-03-21 16:13:20 -0400

    Update GameCard.jsx

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function GameCard() {
   return (
-    <div className="bg-blue-700 p-5 rounded-md shadow-inner shadow-light-100/10">
+    <div className="bg-gray-800 p-5 rounded-md shadow-inner shadow-light-100/10">
       <h1>THIS IS A TEST</h1>
     </div>
   );
