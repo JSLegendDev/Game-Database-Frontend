995a2aec71ee5bdb32c969a0bb52e4df4b242fff
JSLegend <jslegend@protonmail.com>
2025-03-22 18:05:29 -0400

    improving the UI

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -5,14 +5,14 @@ export default function GameCard({ coverLink, name, playtime, genres }) {
     <div className="bg-gray-800 rounded-md">
       <img src={coverLink} className="w-full h-40 object-cover rounded-t-md" />
 
-      <h1 className="p-2 text-gray-300">{name}</h1>
+      <h1 className="p-2 text-gray-300 text-lg">{name}</h1>
       <div className="flex items-center gap-2 mx-2">
         <img src="./clock-fill.svg" className="text-gray-300" />
         <p className=" text-gray-300">{playtime} Hours</p>
       </div>
-      <div className="flex flex-row gap-2 m-2">
+      <div className="flex flex-row flex-wrap gap-2 m-2">
         {genres.map((genre) => (
-          <span className="bg-red-800 rounded-full px-2 text-gray-300">
+          <span className="bg-red-800 rounded-full px-2 text-gray-300 text-sm">
             {genre.name}
           </span>
         ))}
