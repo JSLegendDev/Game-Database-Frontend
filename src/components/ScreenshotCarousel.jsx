8e72bdc102fc6cdfbe3a1cb50e25e0604da6f143
JSLegend <jslegend@protonmail.com>
2025-03-29 18:11:10 -0400

    improved styling of screenshotcarousel

--- src/components/ScreenshotCarousel.jsx
+++ src/components/ScreenshotCarousel.jsx
@@ -8,8 +8,8 @@ export default function ScreenshotCarousel({ screenshots }) {
       className="bg-gray-800 rounded-lg w-full max-w-3xl h-full max-h-96 overflow-hidden relative"
       key={screenshots[currentScreenshotIndex].id}
     >
-      <button
-        className="bg-gray-200 rounded-full px-4 py-2 absolute top-40 right-5 shadow-lg"
+      <img
+        className="bg-gray-200 rounded-full p-4 absolute top-40 right-5 shadow-xl"
         onClick={() => {
           setCurrentScreenshotIndex((currentScreenshotIndex) => {
             if (currentScreenshotIndex < screenshots.length - 1) {
@@ -19,11 +19,10 @@ export default function ScreenshotCarousel({ screenshots }) {
             return 0;
           });
         }}
-      >
-        {">"}
-      </button>
-      <button
-        className="bg-gray-200 rounded-full px-4 py-2 absolute top-40 left-5 shadow-lg"
+        src="./arrow-right.svg"
+      />
+      <img
+        className="bg-gray-200 rounded-full p-4 absolute top-40 left-5 shadow-xl"
         onClick={() => {
           setCurrentScreenshotIndex((currentScreenshotIndex) => {
             if (currentScreenshotIndex > 0) {
@@ -33,9 +32,8 @@ export default function ScreenshotCarousel({ screenshots }) {
             return screenshots.length - 1;
           });
         }}
-      >
-        {"<"}
-      </button>
+        src="./arrow-left.svg"
+      />
 
       <img
         className="object-contain w-full h-full"
